772f6748d9f6fe95fe05aaeae9d01b7e9b613136
Mikhail Solomatin <m.solomatin@gmail.com>
2016-03-27 14:21:29 +0100

    Added unit tests.

--- getdata.py
+++ getdata.py
@@ -8,11 +8,9 @@ import config
 d = config.read_config()
 TCP_IP = config.tcp_ip(d)
 TCP_PORT = config.tcp_port(d)
-#TCP_PORT = 31018
 BUFFER_SIZE = config.buffer_size(d)
 
 print TCP_IP, TCP_PORT, BUFFER_SIZE, "\n"
-received = 0
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect((TCP_IP, TCP_PORT))
---
+++ test/helpertests.py
@@ -0,0 +1,29 @@
+import sys
+sys.path.append("../")
+import unittest
+import helper
+
+class HelperTests(unittest.TestCase):
+    def testOne(self):
+        self.assertEqual(helper.bin2int("0"), 0)
+        self.assertEqual(helper.bin2int("1"), 1)
+        self.assertEqual(helper.bin2int("1111"), 15)
+        self.assertEqual(helper.bin2int("11110"), 30)
+
+    def testTwo(self):
+        self.assertEqual(helper.hex2bin("0"), "0000")
+        self.assertEqual(helper.hex2bin("1"), "0001")
+        self.assertEqual(helper.hex2bin("A"), "1010")
+        self.assertEqual(helper.hex2bin("F"), "1111")
+
+    def testThree(self):
+        self.assertEqual(helper.bin2hex("0"), "0")
+        self.assertEqual(helper.bin2hex("10"), "2")
+        self.assertEqual(helper.bin2hex("1010"), "A")
+        self.assertEqual(helper.bin2hex("1111"), "F")
+
+def main():
+    unittest.main()
+
+if __name__ == '__main__':
+    main()
